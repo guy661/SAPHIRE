b0a14684e6b084dc1cbf1d07ffd87850f88e348e
user <user@example.com>
2025-11-25 20:31:13 +0100

    fix(db): update schema.sql with specification column

--- schema.sql
+++ schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS topics (
     main_topic TEXT NOT NULL,
     include_keywords TEXT,
     exclude_keywords TEXT,
+    specification TEXT,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
