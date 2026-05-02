e74075508e4a69baf82c7af4de76667d1fb9182c
guy661 <navidmardanzai2010@gmail.com>
2026-05-02 16:18:07 +0200

    ang cors fix 2

--- postgres.js
+++ postgres.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('connect', () => {
